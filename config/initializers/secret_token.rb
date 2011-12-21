e1b8ee97cb97a0b4553862083d741ecef78fecb2
Wojtek Majewski <jumski@gmail.com>
2011-12-21 15:08:34 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DekorujeCom::Application.config.secret_token = '767526e8d66f19d4858163b2eee9ecf4fc8fb23e54956f8f40eaaeb8518ddb32fe9e1f38305faab8fa6a815c05be24329f8d775a7515336d4bdfcb4da760a526'
